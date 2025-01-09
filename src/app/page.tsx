af799849a7b95a47ae3868a9135ede4cdc6a4207
Nanasark <danielnyamekyesarkodie@gmail.com>
2025-01-09 18:51:47 +0000

    blue-600

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import BulkEmailSender from "@/components/BulkEmailSender";
 
 export default function Home() {
   return (
-    <main className="container mx-auto p-4">
+    <main className="text-blue-600 bg-white container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Bulk Email Sender</h1>
       <BulkEmailSender />
     </main>
